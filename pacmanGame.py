46c589560c68d3729a98cf70cb4036b5704b87dc
John Hewitt <hewittjd@appstate.edu>
2017-10-07 11:12:21 -0400

    Update pacmanGame.py

--- pacmanGame.py
+++ pacmanGame.py
@@ -1,8 +1,13 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
 
+class PacGame(Widget):
+	
 class PacApp(App):
-  def build(self):
+	def build(self):
     #initialize and return game
+	game = PacGame()
+	return game
     
 if __name__ == '__main__':
-    PacApp().run()
+	PacApp().run()
