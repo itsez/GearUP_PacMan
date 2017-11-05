6900267a0b25fcf90cc3485daa33508eaef1c604
John Hewitt <hewittjd@appstate.edu>
2017-11-05 14:05:42 -0500

    Pacman Animations Update
    Pacman now changes direction visually while still keeping his chomp animation. Also started figuring out how to draw the dots and make them interact.

--- pacmanGame.py
+++ pacmanGame.py
@@ -5,6 +5,7 @@ from kivy.properties import NumericProperty, ReferenceListProperty,\
 from kivy.core.window import Window
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.graphics import Color, Ellipse
 
 
 class Pac(Widget):
@@ -17,33 +18,43 @@ class Pac(Widget):
     end_angle = NumericProperty(230)    # 270 = closed mouth
     bite_down = 1
     bite_speed = 1.5
+    rotation = 0
 
     def move(self,keycode='up'):
         if keycode[1] == 'up':
             self.velocity_y = self.speed
             self.velocity_x = 0
+            self.rotate(90)
         if keycode[1] == 'down':
             self.velocity_y = self.speed * -1
             self.velocity_x = 0
+            self.rotate(-90)
         if keycode[1] == 'left':
             self.velocity_x = self.speed * -1
             self.velocity_y = 0
+            self.rotate(0)
         if keycode[1] == 'right':
             self.velocity_x = self.speed
             self.velocity_y = 0
+            self.rotate(180)
         self.pos = Vector(self.velocity) + self.pos
 
     def chomp(self):
         if self.bite_down == 1:
             self.end_angle += self.bite_speed
             self.start_angle += -self.bite_speed
-            if self.end_angle >= 270:
+            if self.end_angle >= 270 + self.rotation:
                 self.bite_down = 0
         else:
             self.end_angle += -self.bite_speed
             self.start_angle += self.bite_speed
-            if self.end_angle <= 230:
+            if self.end_angle <= 230 + self.rotation:
                 self.bite_down = 1
+
+    def rotate(self, val):
+        self.rotation = val
+        self.start_angle = -50 + val
+        self.end_angle = 230 + val
 
 
 class PacGame(Widget):
@@ -53,6 +64,7 @@ class PacGame(Widget):
         super(PacGame, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        self.draw_dots()
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -64,6 +76,11 @@ class PacGame(Widget):
     def update(self, dt):
         self.pac.chomp()
         self.pac.move()
+
+    def draw_dots(self):
+        with self.canvas:
+            Color(244,244,230)
+            Ellipse(pos=(100, 300), size=(10,10))
 
 
 class PacmanApp(App):
